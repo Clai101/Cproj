de7cb9901076353ce9e0afe92c439b9492b349dc
new_user <new_user@mail.ru>
2021-09-06 07:58:22 +0300

    Добавьте файлы проекта.

---
+++ Cproj/Source.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#define newl '\n'
+#define Tab '\t'
+
+
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d \n", n);
+
+	system("pause");
+	return 0;
+}
